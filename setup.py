0fbf07bf20d873f1d139ad363d262906aa68c24c
GadgetReactor <GadgetReactor@users.noreply.github.com>
2016-10-24 16:47:14 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,32 @@
-from setuptools import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+from distutils.core import setup
+import os
+
+
+def get_packages(package):
+    """
+    Return root package & all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
 
 setup(
     name='fakepyA20GPIO',
